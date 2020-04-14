1f212cb6d83658296639e240912263e8a0f9ff14
Your Name <you@example.com>
2020-04-14 01:46:30 +0100

    Final Submission (Challenge 1 - 3)

--- src/estimator.php
+++ src/estimator.php
@@ -1,6 +1,67 @@
 <?php
 
+/** This function uses the same values for the impact cases
+ *  to update the values of severeImpact cases (i.e values of the impact cases * 5)
+ */  
+function multiplyByFive($value)
+{
+  $value = ($value * 5);
+  return $value;
+}
+
 function covid19ImpactEstimator($data)
 {
+
+  $input = json_decode($data, true);
+
+  $impact = array();
+
+  $severeImpact = array();
+
+  // Estimated number of currently infected.
+  $currentlyInfected = ($input['reportedCases'] * 10);
+
+
+  $infectionsByRequestedTime = ($currentlyInfected * 512);
+
+
+  // Estimated number of severe positive cases. 
+  $severeCasesByRequestedTime = ($infectionsByRequestedTime * 0.15);
+
+
+  $totalHospitalBeds = $input['totalHospitalBeds'];
+
+
+  /* Estimated number of available hospital beds for available cases 
+  * (Note: The below computation is for hospitals with 95% hospital bed capacity).
+  */
+  $hospitalBedsByRequestedTime = ( ( $totalHospitalBeds * (0.35 * 0.95) ) - $severeCasesByRequestedTime );
+
+
+  // Estimated number of severe positive cases that will require ICU care.
+  $casesForICUByRequestedTime = ($infectionsByRequestedTime * 0.05);
+
+
+  // Estimated number of severe positive cases that will require ventilators.
+  $casesForVentilatorsByRequestedTime = ($infectionsByRequestedTime * 0.02);
+
+  $avgDailyIncomeInUSD = $input['region']['avgDailyIncomeInUSD'];
+
+  // Estimated amount of money the economy is likely to lose daily.
+  $dollarsInFlight = round(($infectionsByRequestedTime * $avgDailyIncomeInUSD) / 30);
+
+  // Pushing the required output values to the impact and severeImpact arrays
+  array_push($impact, $currentlyInfected, $severeCasesByRequestedTime, $hospitalBedsByRequestedTime, $casesForICUByRequestedTime, $casesForVentilatorsByRequestedTime, $dollarsInFlight); 
+
+  array_push($severeImpact, $currentlyInfected, $severeCasesByRequestedTime, $hospitalBedsByRequestedTime, $casesForICUByRequestedTime, $casesForVentilatorsByRequestedTime, $dollarsInFlight);
+
+  $severeImpact = array_map("multiplyByFive", $severeImpact);
+
+  $data['input'] = $input;
+  $data['impact'] = $impact;
+  $data['severeImpact'] = $severeImpact;
+
+  $data = json_encode($data);
+
   return $data;
 }
